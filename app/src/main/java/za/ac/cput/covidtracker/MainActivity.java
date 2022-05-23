5431956e26cfca95fc02c4110638dee9356cdd0b
DavinC9903 <220021333@mycput.ac.za>
2022-05-23 23:45:54 +0200

    Statistics Activity

--- app/src/main/java/za/ac/cput/covidtracker/MainActivity.java
+++ app/src/main/java/za/ac/cput/covidtracker/MainActivity.java
@@ -1,8 +1,12 @@
+// Davin Carstens
+// 220021333
 package za.ac.cput.covidtracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +15,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        findViewById(R.id.buttonBarChart).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                startActivity(new Intent(getApplicationContext(), BarChart19.class));
+            }
+        });
+
+        findViewById(R.id.buttonPieChart).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                startActivity(new Intent(getApplicationContext(), PieChart19.class));
+            }
+        });
+
+        findViewById(R.id.buttonRadarChart).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                startActivity(new Intent(getApplicationContext(), RadarChart19.class));
+            }
+        });
 
     }
 }
